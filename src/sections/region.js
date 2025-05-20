faa2aa94f24b1c6ad4041b03e7b9af65fb726876
Michael McAdam <michael@goodnature.co.nz>
2025-05-20 21:58:55 +0200

    Playing around with Coda

--- src/sections/region.js
+++ src/sections/region.js
@@ -3,10 +3,9 @@ import { Chip } from "@mui/material";
 import styled from "@emotion/styled";
 import data from "../data";
 import Section from "../components/Section";
-import { collection, doc, getDocs, query, where } from "firebase/firestore";
 import { levels } from "../util";
 import { recordRegionSelected } from "../analytics";
-import { db } from "../firebase";
+import { fetchCodaRows } from "../coda";
 
 var Scroll = require("react-scroll");
 var scroller = Scroll.scroller;
@@ -18,141 +17,162 @@ const order = levels;
  If there is only one result it "automatically" selects it (which required a little bit of bodging)
 */
 
-let clickHandler = async (state, dispatch, index, db, id) => {
-  const level = levels[index];
+let clickHandler = async (state, dispatch, index, _db, id) => {
+    const level = levels[index];
 
-  // Clear selected state and data below the current level
-  let selected = state.selected;
-  let data = state.data;
-  for (let k = levels.length; k > index; k--) {
-    selected[levels[k]] = "";
-    data[levels[k]] = [];
-  }
-  selected[level] = id;
+    // Clear selected state and data below the current level
+    let selected = state.selected;
+    let data = state.data;
+    for (let k = levels.length; k > index; k--) {
+        selected[levels[k]] = "";
+        data[levels[k]] = [];
+    }
+    selected[level] = id;
 
-  //Update state
-  dispatch({
-    type: "setSelected",
-    payload: { selected, data },
-  });
+    //Update state
+    dispatch({
+        type: "setSelected",
+        payload: { selected, data },
+    });
 
-  // Figure out path based on new state
-  let path = "";
-  for (let j = 0; j <= index; j++) {
-    path += `/${order[j]}s/${selected[order[j]]}`;
-  }
-  path += `/${order[index + 1]}s`;
+    // Figure out next level
+    const nextLevel = order[index + 1];
+    if (!nextLevel) return;
 
-  // Request new data
-  let q = query(collection(db, path), where("include", "==", true));
-  let res = await getDocs(q);
-  let locs = res.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  dispatch({
-    type: "addData",
-    payload: {
-      type: order[index + 1],
-      data: locs,
-    },
-  });
+    // Fetch from Coda.io
+    let locs = [];
+    try {
+        locs = await fetchCodaRows(nextLevel, id);
+    } catch (e) {
+        // Optionally handle error
+        locs = [];
+    }
+    dispatch({
+        type: "addData",
+        payload: {
+            type: nextLevel,
+            data: locs,
+        },
+    });
 
-  // If only one option, select it
-  if (locs.length == 1) {
-    await clickHandler(
-      { ...state, data: { ...state.data, [order[index + 1]]: locs } },
-      dispatch,
-      index + 1,
-      db,
-      locs[0].id
-    );
-  } else if (locs.length == 0) {
-    // If no options, assume we are at the bottom and scroll to next section
-    recordRegionSelected(state.data, state.selected);
-    scroller.scrollTo("who", { smooth: true });
-  }
+    // If only one option, select it
+    if (locs.length == 1) {
+        await clickHandler(
+            { ...state, data: { ...state.data, [nextLevel]: locs } },
+            dispatch,
+            index + 1,
+            _db,
+            locs[0].id
+        );
+    } else if (locs.length == 0) {
+        // If no options, assume we are at the bottom and scroll to next section
+        recordRegionSelected(state.data, state.selected);
+        scroller.scrollTo("who", { smooth: true });
+    }
 };
 
 const render = ({ state, dispatch }) => {
-  let district = state.selected.district;
-  let wardFinder =
-    district && state.data.district.find(({ id }) => id === district)?.wardMap;
+    let district = state.selected.district;
+    let wardFinder =
+        district &&
+        state.data.district.find(({ id }) => id === district)?.wardMap;
 
-  return (
-    <div id="region">
-      <Section>
-        Where are you based?
-        {order.map((level, i) => {
-          // Don't render any options if there are less than 2
-          if (state.data[level].length < 2) return <></>;
-          return (
-            <Container key={level}>
-              <p>{level}</p>
-              <LocationsSection>
-                {state.data[level]
-                  .sort((a, b) => (a.name > b.name ? 1 : -1))
-                  .map((loc) => {
-                    let sel = loc.id == state.selected[level];
+    return (
+        <div id="region">
+            <Section>
+                Where are you based?
+                {order.map((level, i) => {
+                    // Don't render any options if there are less than 2
+                    if (state.data[level].length < 2) return <></>;
                     return (
-                      <Chip
-                        key={loc.name}
-                        label={loc.name}
-                        // color={"primary"}
-                        className="Chip"
-                        color={sel ? "primary" : "secondary"}
-                        sx={{ transform: sel ? "scale(1.2)" : "" }}
-                        onClick={async () =>
-                          await clickHandler(state, dispatch, i, db, loc.id)
-                        }
-                      />
+                        <Container key={level}>
+                            <p>{level}</p>
+                            <LocationsSection>
+                                {state.data[level]
+                                    .sort((a, b) => (a.name > b.name ? 1 : -1))
+                                    .map((loc) => {
+                                        let sel =
+                                            loc.id == state.selected[level];
+                                        return (
+                                            <Chip
+                                                key={loc.name}
+                                                label={loc.name}
+                                                // color={"primary"}
+                                                className="Chip"
+                                                color={
+                                                    sel
+                                                        ? "primary"
+                                                        : "secondary"
+                                                }
+                                                sx={{
+                                                    transform: sel
+                                                        ? "scale(1.2)"
+                                                        : "",
+                                                }}
+                                                onClick={async () =>
+                                                    await clickHandler(
+                                                        state,
+                                                        dispatch,
+                                                        i,
+                                                        null,
+                                                        loc.id
+                                                    )
+                                                }
+                                            />
+                                        );
+                                    })}
+                            </LocationsSection>
+                        </Container>
                     );
-                  })}
-              </LocationsSection>
-            </Container>
-          );
-        })}
-        {wardFinder && (
-          <p>
-            Need{" "}
-            <a href={wardFinder} target="_blank" rel="noopener noreferrer">
-              {" "}
-              help?
-            </a>
-          </p>
-        )}
-      </Section>
-    </div>
-  );
+                })}
+                {wardFinder && (
+                    <p>
+                        Need{" "}
+                        <a
+                            href={wardFinder}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {" "}
+                            help?
+                        </a>
+                    </p>
+                )}
+            </Section>
+        </div>
+    );
 };
 
 export default render;
 
 const Container = styled.div`
-  margin-top: 20px;
-  width: 100%;
-  text-align: center;
+    margin-top: 20px;
+    width: 100%;
+    text-align: center;
 
-  & > p {
-    margin: 0;
-    font-size: 14px;
-    font-weight: bold;
-    text-transform: capitalize;
-    font-style: italic;
-  }
+    & > p {
+        margin: 0;
+        font-size: 14px;
+        font-weight: bold;
+        text-transform: capitalize;
+        font-style: italic;
+    }
 `;
 
 const LocationsSection = styled.div`
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-content: space-between;
-  flex-wrap: wrap;
-  margin-top: 10px;
-  margin-left: auto;
-  margin-right: auto;
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-content: space-between;
+    flex-wrap: wrap;
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
 
-  & > .Chip {
-    /* color: white; */
-    /* color: #221f1f; */
-    margin: 2px 10px;
-  }
+    & > .Chip {
+        /* color: white; */
+        /* color: #221f1f; */
+        margin: 2px 10px;
+    }
 `;
