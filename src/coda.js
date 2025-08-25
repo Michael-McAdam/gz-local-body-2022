748e9b87a80f8b057f093a3746e1860d34198f74
Michael <michael.mcadam@adinnovations.nl>
2025-08-25 13:57:54 +0200

    Built for adding data

--- src/coda.js
+++ src/coda.js
@@ -56,7 +56,7 @@ export const TABLE_IDS = {
 
 // Set to true to use local JSON, false to fetch from Coda and save
 
-export const USE_LOCAL_DATA = true;
+export const USE_LOCAL_DATA = false;
 
 export async function fetchAllTables() {
     if (USE_LOCAL_DATA) {
